3d7873122a98d8d9793cfe129717a45e314a54dd
William Liu <orangetnt827@gmail.com>
2024-11-23 10:32:40 -0600

    test3

--- src/components/details-card/index.tsx
+++ src/components/details-card/index.tsx
@@ -22,7 +22,7 @@ import {
 import { FaSquareThreads } from 'react-icons/fa6';
 import { MdLocationOn } from 'react-icons/md';
 import { RiMailFill, RiPhoneFill } from 'react-icons/ri';
-import { SiResearchgate, SiTwitter, SiUdemy } from 'react-icons/si';
+import { SiResearchgate, SiUdemy } from 'react-icons/si';
 import { Profile } from '../../interfaces/profile';
 import {
   SanitizedGithub,
@@ -208,14 +208,6 @@ const DetailsCard = ({ profile, loading, social, github }: Props) => {
                   title="ResearchGate:"
                   value={social.researchGate}
                   link={`https://www.researchgate.net/profile/${social.researchGate}`}
-                />
-              )}
-              {social?.twitter && (
-                <ListItem
-                  icon={<SiTwitter />}
-                  title="Twitter:"
-                  value={social.twitter}
-                  link={`https://twitter.com/${social.twitter}`}
                 />
               )}
               {social?.mastodon && (
